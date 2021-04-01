a03ea3f5e9044c1ff58309e11b8cf9b2d11e9f60
rxjas <christopherrojas35@yahoo.com>
2021-04-01 15:36:05 -0600

    added the ability to due posts

---
+++ app/models/Vote.py
@@ -0,0 +1,8 @@
+from app.db import Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+class Vote(Base):
+    __tablename__ = 'votes'
+    id = Column(Integer, primary_keys=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
